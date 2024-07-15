f5b7dbb139ffae26c6daf80cab62f03ed1322cb7
Fredy <fredy.castaneda.s21@gmail.com>
2024-07-15 15:23:49 -0600

    Password Hashes
    Place the function that is applyed to convert the plain text to hashes for password

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,6 +2,10 @@
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
 const accountController = {}
+// Password hash
+const bcrypt = require("bcryptjs")
+
+
 
 /* ****************************************
 *  Deliver login view
@@ -33,13 +37,34 @@ accountController.buildRegister = async function (req, res, next) {
 accountController.registerAccount = async function (req, res) {
     let nav = await utilities.getNav()
     const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+    // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
+
+
   
     const regResult = await accountModel.registerAccount(
       account_firstname,
       account_lastname,
       account_email,
-      account_password
+      hashedPassword
     )
+    
+
+
+
   
     if (regResult) {
       req.flash(
@@ -49,12 +74,14 @@ accountController.registerAccount = async function (req, res) {
       res.status(201).render("account/login", {
         title: "Login",
         nav,
+        errors: null,
       })
     } else {
       req.flash("notice", "Sorry, the registration failed.")
       res.status(501).render("account/register", {
         title: "Registration",
         nav,
+        errors: null,
       })
     }
   }
